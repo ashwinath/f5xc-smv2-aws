d46b44ace9cec9366f09341cb92b9c24b27c0d4a
Marcel Wiget <mwiget@gmail.com>
2024-09-13 16:45:02 +0200

    open webui access on port 65500

--- vpc.tf
+++ vpc.tf
@@ -99,6 +99,13 @@ resource "aws_security_group" "allow_slo_traffic" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = "65500"
+    to_port     = "65500"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = "0"
     to_port     = "0"
